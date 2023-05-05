ebb805b2ae1121547b851fc569d9cdf7b11307ef
nabilesall <nabilesall@gmail.com>
2023-05-05 13:46:20 +0200

    mettre le server.js dans le git

---
+++ server.js
@@ -0,0 +1,18 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
+const api_1 = __importDefault(require("./api"));
+const http_1 = require("http");
+const app = (0, express_1.default)();
+app.use(express_1.default.json());
+app.use((0, cors_1.default)());
+app.use('/api', api_1.default);
+const port = 3000;
+const server = (0, http_1.createServer)(app);
+server.listen(port, () => {
+    console.log(`Server running`);
+});
