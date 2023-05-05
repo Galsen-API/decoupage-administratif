31cc7a954807e77597824f05ed1e85de249e31ca
nabilesall <nabilesall@gmail.com>
2023-05-05 14:17:26 +0200

    ajout index de api

---
+++ api/index.js
@@ -0,0 +1,10 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const departments_1 = __importDefault(require("./routes/departments"));
+const router = express_1.default.Router();
+router.use('/departments', departments_1.default);
+exports.default = router;
