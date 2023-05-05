5670e21b1d68ceb888bd2bfbdd8078ac1244c4d8
nabilesall <nabilesall@gmail.com>
2023-05-05 15:19:54 +0200

    changement de department json en ts

---
+++ data/departments.js
@@ -0,0 +1,33 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.departments = void 0;
+exports.departments = [
+    {
+        "name": "Pikine",
+        "region_code": 1
+    },
+    {
+        "name": "Bambey",
+        "region_code": 3
+    },
+    {
+        "name": "Mbour",
+        "region_code": 10
+    },
+    {
+        "name": "Pikine",
+        "region_code": 1
+    },
+    {
+        "name": "Bambey",
+        "region_code": 3
+    },
+    {
+        "name": "Mbour",
+        "region_code": 10
+    },
+    {
+        "name": "Diourbel",
+        "region_code": 2
+    }
+];
