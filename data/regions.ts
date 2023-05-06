052660950c655d19bbebd1cf07ff2adc278b7a30
nabilesall <nabilesall@gmail.com>
2023-05-06 12:43:09 +0200

    ajout de régions

--- data/regions.ts
+++ data/regions.ts
@@ -3,12 +3,19 @@ export const regions = [
         "name" : "Dakar",
         "code" : 1,
         "superficie" : 550,
-        "population" : 4042225
+        "population" : 404_2225
+    },
+    {
+        "name" : "Ziguinchor",
+        "code" : 2,
+        "superficie" : 73399,
+        "population" : 437_986
+
     },
     {
         "name" : "Thies",
         "code" : 7,
         "superficie" : 6601,
-        "population" : 	1442338
+        "population" : 	1442_338
     }
 ]
