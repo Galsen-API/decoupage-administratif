6e6e34c5e5c0edff15be42eeaa145e4bb0bfd93e
nabilesall <nabilesall@gmail.com>
2023-05-27 20:28:00 +0200

    port server

--- server.ts
+++ server.ts
@@ -6,10 +6,10 @@ import { createServer } from 'http';
 const app = express();
 app.use(express.json());
 app.use(cors());
-app.use('/api', api);
+app.use('/decoupage-administratif', api);
 
-const port = 3000;
+const port = 8000;
 const server = createServer(app);
 server.listen(port, () => {
-  console.log(`Server running`);
+  console.log(`Server running at http://localhost:${port}/decoupage-administratif/regions`);
 });
